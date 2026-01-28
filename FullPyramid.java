2a8e44aa07fcf450daf9552e2f4f6a90843ac546
Deepa Lakshmi <chidambaramdeepa66@gmail.com>
2026-01-28 15:35:24 +0530

    Add files via upload

---
+++ FullPyramid.java
@@ -0,0 +1,12 @@
+public class FullPyramid {
+    public static void main(String[] args){
+        for(int i=4;i>=1;i--) {
+            for (int j =1;j<=i; j++) {
+                System.out.print("*");
+            }
+            //for(int k=)
+
+            System.out.println();
+        }
+    }
+}
